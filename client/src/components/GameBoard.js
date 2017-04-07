931dcba6aaf012054fa59a3f514ddc434135e8dc
Emmanuel Vo <emmanuelvo777@gmail.com>
2017-04-07 15:33:04 -0700

    Removed one button and created pseudo button functionality

--- client/src/components/GameBoard.js
+++ client/src/components/GameBoard.js
@@ -1,17 +1,23 @@
 import React from 'react';
 
 class GameBoard extends React.Component {
+  onSubmit(e) {
+    e.preventDefault();
+    console.log("Able to submit!");
+  }
+
   render() {
     return (
       <div className="search-form">
-        <form>
-          <label>Game</label>
-          <input placeholder="Type game name"/>
-          <button className="btn btn-primary">Submit Game</button>
+        <form onSubmit={this.onSubmit}>
+          <div className="form-group">
+            <label>Game</label>
+            <input type="text" ref="game" placeholder="Type game name"/>
 
-          <label>Time Played</label>
-          <input placeholder="Typed estimated time"/>
-          <button className="btn btn-primary">Submit Game</button>
+            <label>Time Played</label>
+            <input type="text" ref="time" placeholder="Typed estimated time"/>
+          </div>
+          <button className="btn btn-primary">Submit</button>
         </form>
       </div>
     );
