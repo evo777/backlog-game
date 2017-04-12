bcbbaed750255726f9401391e77458fee5b6568c
Emmanuel Vo <emmanuelvo777@gmail.com>
2017-04-12 12:04:04 -0700

    Created title for game board

--- client/src/components/GameBoard.jsx
+++ client/src/components/GameBoard.jsx
@@ -1,24 +1,11 @@
 import React from 'react';
 
 class GameBoard extends React.Component {
-  onSubmit(e) {
-    e.preventDefault();
-    console.log("Able to submit!");
-  }
 
   render() {
     return (
-      <div className="search-form">
-        <form onSubmit={this.onSubmit}>
-          <div className="form-group">
-            <label>Game</label>
-            <input type="text" ref="game" placeholder="Type game name"/>
-
-            <label>Time Played</label>
-            <input type="text" ref="time" placeholder="Typed estimated time"/>
-          </div>
-          <button className="btn btn-primary">Submit</button>
-        </form>
+      <div>
+        <h1>My Games</h1>
       </div>
     );
   }
