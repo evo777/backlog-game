197ccd5d57d1ead9588338a11e0096936a85b2c7
Emmanuel Vo <emmanuelvo777@gmail.com>
2017-04-16 00:59:27 -0700

    Passed sample data and able to render it

--- client/src/components/GameBoard.jsx
+++ client/src/components/GameBoard.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 
 class GameBoard extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
   render() {
     return (
       <div>
-        <h1>My Games</h1>
+        <h1>My Game Board</h1>
+        <ul>
+          {this.props.games}
+        </ul>
       </div>
     );
   }
