5e2e5b03e2c5b23623869d8d0fb4880f47fbc684
Emmanuel Vo <emmanuelvo777@gmail.com>
2017-04-23 15:15:10 -0700

    Created Walkthrough component

---
+++ client/src/components/Walkthrough.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Walkthrough extends React.Component {
+  render() {
+    return (
+      <div>Walkthrough Room</div>
+    );
+  }
+}
+
+export default Walkthrough;
