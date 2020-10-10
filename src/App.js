df39bcbe3ce434997262f6f02e541e7fd5400c63
Emmanuel Vo <emmanuelvo777@gmail.com>
2020-10-09 18:11:48 -0700

    Add submit form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [value, setValue] = useState("");
+  function submitGame(e) {
+    console.log("form submitted");
+    // Handle ajax call here to save game item
+    e.preventDefault();
+  }
+
+  function handleChange(e) {
+    setValue(e.target.value);
+  }
+
   return (
     <div className="App">
-      <h1>Under construction</h1>
-      <h2>Add Backlog here:</h2>
+      <h1>Backlog Game (Under Construction)</h1>
+      <form onSubmit={submitGame}>
+        <label>
+          Game:
+          <input type="text" value={value} onChange={handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
     </div>
   );
 }
