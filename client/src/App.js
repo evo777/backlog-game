f9befb51d51c228b48815b4bb9fe644d60f94f7a
Emmanuel Vo <emmanuelvo777@gmail.com>
2021-01-10 20:56:20 -0800

    Set up initial game saving

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
 import React, { useState } from "react";
+import axios from 'axios';
 
 function App() {
   const [value, setValue] = useState("");
   function submitGame(e) {
-    console.log("form submitted");
     // Handle ajax call here to save game item
     e.preventDefault();
+    axios.post('http://localhost:5000/add', { title: value })
+      .then(res => {
+        console.log("I am data: ", res.data);
+      })
+      .catch(e => console.error(e));
   }
 
   function handleChange(e) {
--- index.js
+++ index.js
@@ -1,10 +1,25 @@
-const express = require('express');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get('/', function (req, res, next) {
   res.send('Hello! Backlog Game is under construction.');
+});
+
+app.post('/add', (req, res, next) => {
+  // Save to database
+  try {
+    console.log("req.body: ", req.body);
+  } catch (e) {
+    res.send(400).send("unable to save to database");
+  }
 });
 
 app.listen(PORT, () => {
